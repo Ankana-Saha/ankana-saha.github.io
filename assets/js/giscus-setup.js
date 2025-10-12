2685c6b9878055176121700961bda880b466c7af
fraktur-raptor <muj.ie.me@gmail.com>
2025-10-12 19:57:36 +0000

    Deploying to gh-pages from @ Ankana-Saha/ankana-saha.github.io@a249fb3ead6792e0fcc526d488b4c561719ec1da 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Ankana-Saha/site",
+    "data-repo": "Ankana-Saha/ankana-saha.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
