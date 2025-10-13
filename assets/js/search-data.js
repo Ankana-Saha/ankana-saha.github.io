4653b69e393430f2830b4f6a9711fd8e23b93a39
Ankana-Saha <78070287+Ankana-Saha@users.noreply.github.com>
2025-10-13 23:36:35 +0000

    Deploying to gh-pages from @ Ankana-Saha/ankana-saha.github.io@d805c23ce59a6396ebeb9a02c160cef875719e8f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -66,12 +66,7 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-talk-at-elm",
+      },{id: "news-talk-at-elm",
           title: 'Talk at ELM',
           description: "",
           section: "News",handler: () => {
