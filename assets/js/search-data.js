4af3ee1b493a6fe2fb496b13dc016afa46e6555e
Ankana-Saha <78070287+Ankana-Saha@users.noreply.github.com>
2025-10-14 02:32:14 +0000

    Deploying to gh-pages from @ Ankana-Saha/ankana-saha.github.io@f230714ff163f0252cfafd24699160ffe4768e18 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,49 +44,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
-      },{id: "news-talk-at-elm",
-          title: 'Talk at ELM',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_4/";
-            },},{id: "news-harvard-linguistics-at-sub29",
-          title: 'Harvard Linguistics at SuB29',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_3/";
-            },},{id: "news-poster-presentation-at-xprag-it",
-          title: 'Poster presentation at XPRAG.it',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-paper-accepted-at-praglm-colm-2025",
-          title: 'Paper accepted at PragLM @ COLM 2025',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_1/";
-            },},{id: "projects-demonstrative-descriptions-and-anti-uniqueness",
+        },{id: "projects-demonstrative-descriptions-and-anti-uniqueness",
           title: 'Demonstrative Descriptions and Anti-Uniqueness',
           description: "",
           section: "Projects",handler: () => {
